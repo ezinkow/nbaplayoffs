4e78273df0b0a6dbf819bba1dd5666539d2e0d72
Elan Zinkow <ezinkow@gmail.com>
2023-04-26 23:17:40 -0500

    rnd 2

--- client/src/components/Steps.js
+++ client/src/components/Steps.js
@@ -6,13 +6,25 @@ export default function Steps() {
 
     return (
         <>
-            <h3>Steps:</h3>
+            <h1>Round 2:</h1>
+            <h3>Because <a className='bold'>round 2</a> will likely start while <a className='bold'>round 1</a> is still going on or finishing, all series that are set/locked at the tip time of the first game 1 of round 2 will be locked in at that time.
+            </h3>
+            <p>If a series is ending as game 1 is starting, we'll have some buffer time TBD</p>
+            <h3>
+                <ul>
+                    <li>You can make and change your picks until they lock.</li>
+                    <li>I will accept the last picks you submit.</li>
+                    <li>Please resubmit all 4 series.</li >
+                </ul >
+            </h3>
+            <p>==================</p>
             <ul>
-                <li>Select CORRECT name from drop down</li>
-                <li>Make your picks</li>
+                <li>If something breaks, or seems to be broken, just refresh</li>
+                <li>Select CORRECT NAME from drop down</li>
+                <li>Make your picks. Pick team/points/games in that order please.</li>
                 <li>Picks will show up in the table down below</li>
-                <li>When ready, submit your picks. If you need to make a change, just reach out to me. Don't re-submit picks</li>
-                <li>The google doc will still house your picks and results</li>
+                <li>When ready, submit your picks. Please keep a temporary record.</li>
+                <li>The google doc will house the official picks and results</li>
                 <li>Correctly guessing the number of games in a series will give you a 2x bonus</li>
             </ul>
         </>
