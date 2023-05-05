ef29508528a45f6c4d93ffc1ee62a515950aebde
Elan Zinkow <ezinkow@gmail.com>
2023-05-05 08:28:52 -0500

    rnd 3

--- client/src/components/Steps.js
+++ client/src/components/Steps.js
@@ -5,19 +5,6 @@ export default function Steps() {
 
 
     return (
-        <>
-            <h1>Round 2:</h1>
-            <h3>Because <a className='bold'>round 2</a> will likely start while <a className='bold'>round 1</a> is still going on or finishing, all series that are set/locked at the tip time of the first game 1 of round 2 will be locked in at that time.
-            </h3>
-            <p>If a series is ending as game 1 is starting, we'll have some buffer time TBD</p>
-            <h3>
-                <ul>
-                    <li>You can make and change your picks until they lock.</li>
-                    <li>I will accept the last picks you submit.</li>
-                    <li>Please resubmit all 4 series.</li >
-                </ul >
-            </h3>
-            <p>==================</p>
             <ul>
                 <li>If something breaks, or seems to be broken, just refresh</li>
                 <li>Select CORRECT NAME from drop down</li>
@@ -27,6 +14,5 @@ export default function Steps() {
                 <li>The google doc will house the official picks and results</li>
                 <li>Correctly guessing the number of games in a series will give you a 2x bonus</li>
             </ul>
-        </>
     )
 }
